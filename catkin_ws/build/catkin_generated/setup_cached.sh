71dbe18fb2f478b5df94b2d2d687a39e70683e4b
ozzie <r.hotter66@gmail.com>
2019-11-10 11:53:06 -0500

    turtlebot3 simulation

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/ozzie/ozzie/catkin_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD="/home/ozzie/ozzie/catkin_ws/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/ozzie/ozzie/catkin_ws/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/ozzie/catkin_ws/src:/home/ozzie/ozzie/catkin_ws/src:$ROS_PACKAGE_PATH"
